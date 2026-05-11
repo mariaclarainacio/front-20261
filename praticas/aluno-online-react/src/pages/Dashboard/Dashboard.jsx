3b47108016e6631cf3df62023d116de6c40f8499
unknown <2424290042@ACADEMICO.IESB.BR>
2026-05-11 09:27:55 -0300

    feat: conclui pratica06. Fecha#6

--- praticas/aluno-online-react/src/pages/Dashboard/Dashboard.jsx
+++ praticas/aluno-online-react/src/pages/Dashboard/Dashboard.jsx
@@ -5,7 +5,7 @@ function Dashboard() {
     <div className="pagina-conteudo">
       <h1>Bem-vinda, Maria Clara!</h1>
       <div className="cards-resumo">
-        <div className="card"><h3>Notas</h3><p>Média Global: 8.5</p></div>
+        <div className="card"><h3>Notas</h3><p>Média Geral: 8.5</p></div>
         <div className="card"><h3>Faltas</h3><p>Total: 6</p></div>
         <div className="card"><h3>Avisos</h3><p>2 novos requerimentos</p></div>
       </div>
